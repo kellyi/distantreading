21232495fab618d6e0f753e8085cbf23a036f051
Kelly Innes <kinnes@gmail.com>
2017-09-25 19:27:51 -0400

    Add command line argument parser & placeholders

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
+-- optionparse setup from https://github.com/pcapriotti/optparse-applicative#quick-start
+import Options.Applicative
+import Data.Semigroup ((<>))
+
 import Lib
 
+data Opt = Opt { server :: Bool }
+
+optarg :: Parser Opt
+optarg = Opt
+      <$> switch
+          ( long "server"
+         <> short 's'
+         <> help "Run server instead of command line interface" )
+
 main :: IO ()
-main = someFunc
+main = parse =<< execParser opts
+  where
+    opts = info (optarg <**> helper)
+      ( fullDesc
+     <> progDesc "Generate statistics about a text file"
+     <> header "distantreading" )
+
+parse :: Opt -> IO ()
+parse (Opt True) = putStrLn $ "Placeholder for running the server!"
+parse _ = putStrLn $ "Placeholder for running command line interface!"
